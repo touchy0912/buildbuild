fefc4d973ebfe0ffd12d3339f29ab824b028ceb3
EC2 Default User <ec2-user@ip-172-31-36-109.us-east-2.compute.internal>
2018-07-06 05:59:37 +0000

    add grandrule

---
+++ resources/views/grandrule.blade.php
@@ -0,0 +1,21 @@
+@extends('layouts.app')
+
+@section('content')
+        <div class="">
+            <p>これからみなさんには課題に取り組んでいただきます。<br>
+            その上で以下のルールを守ってください。</p>
+        </div>
+        <div>
+            <ul>
+                <li>ルール１<br>
+                最終的にチームで１つの結論を出してもらいます。</li>
+                <li>ルール２<br>
+                携帯、身の回りの物はしまってください。</li>
+                <li>ルール３<br>
+                みなさんはチームです。相手へのリスペクトを持ち、ネガティブな発言は避けてください。</li>
+            </ul>
+        </div>
+        <div>
+            <a href="#" class="btn btn-default">次へ</a>
+        </div>
+@endsection
