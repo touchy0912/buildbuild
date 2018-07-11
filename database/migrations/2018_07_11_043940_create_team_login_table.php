c120afcb51eb7674dfaa3fd1e9b9418987621a0f
touchy0912 <touchy0912@gmail.com>
2018-07-11 04:53:42 +0000

    login

--- database/migrations/2018_07_11_043940_create_team_login_table.php
+++ database/migrations/2018_07_11_043940_create_team_login_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateDatasavesTable extends Migration
+class CreateTeamLoginTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,11 +13,11 @@ class CreateDatasavesTable extends Migration
      */
     public function up()
     {
-        Schema::create('datasaves', function (Blueprint $table) {
+        Schema::create('teams', function (Blueprint $table) {
             $table->increments('id');
-            $table->string('role');
-            $table->string('rule1');
-            $table->string('rule2');
+            $table->string('name');
+            $table->string('password');
+            $table->rememberToken();
             $table->timestamps();
         });
     }
@@ -29,6 +29,6 @@ class CreateDatasavesTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('datasaves');
+        Schema::dropIfExists('teams');
     }
 }
