f8f8035efc94914794a537044b67a5b6b12956e1
coke <gaku.shoji0516@gmail.com>
2018-07-11 07:36:45 +0000

    bootstrap toppage

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -8,6 +8,8 @@
         <title>Buildbuild</title>
         
         <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+        <link rel="stylesheet" type="text/css" href="/css/buildbuild.css">
+        <link href="https://fonts.googleapis.com/css?family=IM+Fell+DW+Pica+SC rel="stylesheet">
         <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
         <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
 
--- resources/views/pages/toppage.blade.php
+++ resources/views/pages/toppage.blade.php
@@ -4,8 +4,9 @@
 @section('content')
 <div class='text-center'>
 
-<h1>Build-build</h1>
-<h3>ビルビル</h3>
+<h3 class="title2">ビル　　　　　　ビル</h3>
+<h1 class="title1">Build-Build</h1>
+
 
 
 
@@ -16,37 +17,38 @@
     {!! Form::open(['route'=>'games.grandrule']) !!}
     
     <div class='form-group'>
-    {!! Form::label('user1','一人目') !!}
-    {!! Form::text('user1',old('user1'),['class'=>'form-control']) !!}
+    {!! Form::label('user1','PLAYER 1') !!}
+    {!! Form::text('user1',old('user1'), ['class'=>'form-control', 'id'=>'focusedInput']) !!}
     </div>
     
-        <div class='form-group'>
-    {!! Form::label('user2','二人目') !!}
+    <div class='form-group'>
+    {!! Form::label('user2','PLAYER 2') !!}
     {!! Form::text('user2',old('user2'),['class'=>'form-control']) !!}
     </div>
     
-        <div class='form-group'>
-    {!! Form::label('user3','三人目') !!}
+    <div class='form-group'>
+    {!! Form::label('user3','PLAYER 3') !!}
     {!! Form::text('user3',old('user3'),['class'=>'form-control']) !!}
     </div>
     
-        <div class='form-group'>
-    {!! Form::label('user4','四人目') !!}
+    <div class='form-group'>
+    {!! Form::label('user4','PLAYER 4') !!}
     {!! Form::text('user4',old('user4'),['class'=>'form-control']) !!}
     </div>
     
         <div class='form-group'>
-    {!! Form::label('user5','五人目') !!}
+    {!! Form::label('user5','PLAYER 5') !!}
     {!! Form::text('user5',old('user5'),['class'=>'form-control']) !!}
     </div>
     
         <div class='form-group'>
-    {!! Form::label('user6','六人目') !!}
+    {!! Form::label('user6','PLAYER 6') !!}
     {!! Form::text('user6',old('user6'),['class'=>'form-control']) !!}
     </div>
     
-    {!! Form::submit('START',['class'=>'btn btn-primary btn-block']) !!}
-    
+    <div class='botton'>
+    {!! Form::submit("LET'S GET START!",['class'=>'btn btn-block']) !!}
+    </div>
 
     
     {!! Form::close() !!}
