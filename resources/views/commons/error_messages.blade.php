624a594aa19f750ec2afb5f8d1e214836066afb4
nakkygyo <natsukigyotoku.yahoo.co.jp>
2018-07-12 04:41:14 +0000

    modify error messages and grand rule

--- resources/views/commons/error_messages.blade.php
+++ resources/views/commons/error_messages.blade.php
@@ -1,9 +1,11 @@
 @if(count($errors)>0)
-@foreach($errors->all() as $error)
 <div class='alert alert-warnig'>
-    <ul>
+    <ul>  
+@foreach($errors->all() as $error)
+
     <li>{{$error}}</li>
+    
+@endforeach
     </ul>
 </div>
-@endforeach
 @endif
