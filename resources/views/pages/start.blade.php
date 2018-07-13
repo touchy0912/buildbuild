17d8125a52635627d19ba25b086a0ae1c26a92b6
touchy0912 <touchy0912@gmail.com>
2018-07-13 03:06:38 +0000

    red fonts

--- resources/views/pages/start.blade.php
+++ resources/views/pages/start.blade.php
@@ -5,7 +5,7 @@
 
 <div class="rulepage">
 
-<h1>スタート</h1>
+<h1>スタート!!</h1>
 
 <?php $i=1 ?>
 @foreach($themas as $thema)
@@ -14,6 +14,11 @@
 @endif
 <?php $i++; ?>
 @endforeach
+
+<br>
+
+<h1>制限時間は<span class="grandrule">５分間</span>です！</h1>
+
 
 {!! link_to_route("thema.feedback","ゲーム終了",'',['class'=>'btn btn-default']) !!}
 
